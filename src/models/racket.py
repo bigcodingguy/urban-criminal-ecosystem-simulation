8d62ac38e77d78af2e07dbf4823cddcb4207028d
Blake Hammontree <blakehammontree@Blakes-MacBook-Air.local>
2026-02-26 18:28:12 -0500

    Add Racket class with income calculation

---
+++ src/models/racket.py
@@ -0,0 +1,15 @@
+import random
+
+class Racket:
+    def __init__(self, type, base_income, heat_generated):
+        self.type = type
+        self.territory = None
+        self.base_income = base_income
+        self.heat_generated = heat_generated
+        self.is_operational = True
+
+    def calculate_income(self):
+        if self.is_operational:
+            return self.base_income
+        else:
+            return 0
